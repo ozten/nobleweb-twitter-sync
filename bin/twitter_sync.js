a163d56f772657872cf42421f85b73c3f8e1ae14
Austin King <shout@ozten.com>
2013-11-22 14:09:20 -0800

    First time import of tweets, write to the file system.

--- bin/twitter_sync.js
+++ bin/twitter_sync.js
@@ -1,13 +1,24 @@
 #!/usr/local/bin/node
+
+/**
+ * Run for a given user
+ */
+
+// Rate limiting library
+
 var fs = require('fs');
 var path = require('path');
 
 var twitter = require('twitter');
 
+var rateLimit = require('../lib/rate_limit');
+
 var config = JSON.parse(fs.readFileSync(
     path.join(__dirname, '../config/config.json')));
 
-console.log('Twitter sync');
+const MAX_TWEETS_PER_PAGE = 200;
+const MAX_TIMELINE_CALLS = 180;
+const FIVETEEN_MIN = 15 * 60; // in seconds
 
 var twit = new twitter({
     consumer_key: config.consumer_key,
@@ -16,12 +27,66 @@ var twit = new twitter({
     access_token_secret: null
 });
 
-twit.getUserTimeline({
-    screen_name: 'ozten'
-}, function(err, tweets, a, b) {
-    console.log('err=', err);
-    console.log('tweets=', tweets);
-    console.log('a=', a, 'b=', b);
-});
+var limited = rateLimit(FIVETEEN_MIN, MAX_TIMELINE_CALLS);
 
-console.log(twit);
+function fullSync(screenName) {
+    limited(function() {
+        var params = {
+            screen_name: screenName
+        };
+        // Full Sync
+        twit.getUserTimeline(params, function(tweets) {
+            saveTweets(params, tweets);
+            console.log('tweets=', tweets);
+            var lowestId = findLowestId(tweets);
+            if (lowestId > 0) {
+                limited(function() {
+                    fetchPage(screenName, lowestId - 1, MAX_TWEETS_PER_PAGE);
+                });
+            }
+        });
+    });
+}
+
+/**
+ * If there are no lower ids (because results were empty)
+ * code returns -1
+ */
+function findLowestId(tweets) {
+    var lowest = -1;
+
+    tweets.forEach(function(tweet) {
+        if (lowest === -1 || tweet.id < lowest) {
+            lowest = tweet.id;
+        }
+    });
+    console.log('Lowest Tweet ID seen is', lowest);
+    return lowest;
+}
+
+function saveTweets(params, tweets) {
+    var start = params.max_id || 'start';
+    var filename = params.screen_name + '_' + start + '.json';
+    fs.writeFile(filename, JSON.stringify(tweets, null, 4), 'utf8');
+}
+
+function fetchPage(screenName, maxId, count) {
+    var params = {
+        screen_name: screenName,
+        max_id: maxId,
+        count: count
+    };
+    twit.getUserTimeline(params, function(tweets) {
+
+        saveTweets(params, tweets);
+
+        var lowestId = findLowestId(tweets);
+
+        if (lowestId > 0) {
+            limited(function() {
+                fetchPage(screenName, lowestId - 1, MAX_TWEETS_PER_PAGE);
+            });
+        }
+    });
+}
+fullSync('ozten');
